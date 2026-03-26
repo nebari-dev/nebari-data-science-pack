b9c8287c79c41706e839241d4aa61f4c11c93748
Amit Kumar <aktech@users.noreply.github.com>
2026-03-26 22:03:23 +0000

    fix: use python for index.html injection (sed breaks on multiline)

--- images/scripts/inject-font.sh
+++ images/scripts/inject-font.sh
@@ -7,23 +7,14 @@ set -euo pipefail
 
 INDEX_HTML="${1:?Usage: inject-font.sh <path-to-index.html>}"
 
-INJECTION='<link rel="preload" href="../static/lab/fonts/FiraCodeNerdFontMono-Regular.ttf" as="font" type="font/ttf" crossorigin>
-<style>
-@font-face {
-  font-family: "FiraCode Nerd Font";
-  src: url("../static/lab/fonts/FiraCodeNerdFontMono-Regular.ttf") format("truetype");
-  font-weight: 400;
-  font-display: block;
-}
-@font-face {
-  font-family: "FiraCode Nerd Font";
-  src: url("../static/lab/fonts/FiraCodeNerdFontMono-Bold.ttf") format("truetype");
-  font-weight: 700;
-  font-display: block;
-}
-</style>
-<script>document.fonts.ready.then(function(){setTimeout(function(){window.dispatchEvent(new Event("resize"))},100)})</script>'
-
-# Insert before </head>
-sed -i "s|</head>|${INJECTION}</head>|" "$INDEX_HTML"
-echo "Font injection complete: $INDEX_HTML"
+python3 -c "
+import sys
+path = sys.argv[1]
+injection = '''<link rel=\"preload\" href=\"../static/lab/fonts/FiraCodeNerdFontMono-Regular.ttf\" as=\"font\" type=\"font/ttf\" crossorigin><style>@font-face{font-family:\"FiraCode Nerd Font\";src:url(\"../static/lab/fonts/FiraCodeNerdFontMono-Regular.ttf\") format(\"truetype\");font-weight:400;font-display:block}@font-face{font-family:\"FiraCode Nerd Font\";src:url(\"../static/lab/fonts/FiraCodeNerdFontMono-Bold.ttf\") format(\"truetype\");font-weight:700;font-display:block}</style><script>document.fonts.ready.then(function(){setTimeout(function(){window.dispatchEvent(new Event(\"resize\"))},100)})</script>'''
+with open(path, 'r') as f:
+    html = f.read()
+html = html.replace('</head>', injection + '</head>', 1)
+with open(path, 'w') as f:
+    f.write(html)
+print(f'Font injection complete: {path}')
+" "$INDEX_HTML"
